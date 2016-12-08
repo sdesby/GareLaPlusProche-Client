ad49a73809aebdc90eaadaa2197c904fd136d800
sdesby <desby.stephanie@gmail.com>
2016-12-08 21:31:36 +0100

    Handle unknown address error

--- app/api_services.py
+++ app/api_services.py
@@ -12,22 +12,34 @@ def get_nearest_train_station(address):
     try:
         response = urlopen(request)
         station = json.loads(response.read())
-        print "Reading response"
-        #m_id = station["_id"]["$oid"]
-        name = station["name"].encode("utf-8")
-        howbig = station["howbig"].encode("utf-8")
-        latitude = station["latitude"]
-        longitude = station["longitude"]
-        postal_code = station["postalCode"]
-        city = station["city"].encode("utf-8")
-        department= station["department"].encode("utf-8")
-        region =station["region"].encode("utf-8")
-        station = TrainStation()
-        station.initialize("1", name, howbig, latitude, longitude, postal_code, city, department, region)
-        return station
+
+        print "#####"
+        print station
+        print "#####"
+        if "error" not in station:
+            print "Reading response"
+            #m_id = station["_id"]["$oid"]
+            name = station["name"].encode("utf-8")
+            howbig = station["howbig"].encode("utf-8")
+            latitude = station["latitude"]
+            longitude = station["longitude"]
+            postal_code = station["postalCode"]
+            city = station["city"].encode("utf-8")
+            department= station["department"].encode("utf-8")
+            region =station["region"].encode("utf-8")
+            station = TrainStation()
+            station.initialize("1", name, howbig, latitude, longitude, postal_code, city, department, region)
+            return station
+        else:
+            return json.dumps(station)
+
 
     except URLError, e:
-		print "Erreur lors de la communication avec le service distant :" , e
-		return -1
+        print e
+        if "HTTP Error 400" in str(e):
+		    return 400
+        else:
+            return -1
+
 if __name__ == "__main__":
     get_nearest_train_station("14 rue de vignolle, 51120 saint-quentin-le-verger")
--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ from app import app
 
 from .models import TrainStation
 from .form import AddressForm
+import json
 
 import api_services
 
@@ -14,11 +15,11 @@ def index():
 	if form.validate_on_submit():
 		address=form.address.data.encode("utf-8")
 		station = api_services.get_nearest_train_station(address)
-		print "Station in views have been initialize"
-
-		if isinstance(station, str):
-			error = station
-			return render_template('index.html', title='Nearest station', form=form, address=address, station=None, error=error)
+		
+		if isinstance(station, int):
+			print station
+			if station == 400:
+				return render_template('index.html', title='Nearest station', form=form, address=address, station=None, error="HTTP 400: Bad request. Please check that address exists")
 		else:
 			return render_template('index.html', title='Nearest station', form=form, address=address.decode("utf-8"), station=station, error="")
 	return render_template('index.html', title='Nearest station', form=form, address="", station="", error="")
